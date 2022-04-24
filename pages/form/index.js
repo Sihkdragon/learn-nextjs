df85347de8882b69e0b7f24f5670e73beb718e9d
Sihkdragon <dithor37@gmail.com>
2022-04-24 13:59:04 +0700

    add some feature like

--- pages/form/index.js
+++ pages/form/index.js
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import TopItems from "../../components/molecules/TopItems";
+import Header from "../../components/organism/Header";
 const index = () => {
   const {
     register,
@@ -9,20 +11,45 @@ const index = () => {
   const onSubmit = (data) => console.log(data);
 
   console.log(watch("example")); // watch input value by passing the name of it
-
+  let today = new Date();
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+    <>
+      <Header title="Form" />
+      <TopItems bigTitleContent="Form" />
+      <h3 className="text-lg text-indigo-600 my-5">Biodata Form</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col space-y-2">
+          {/* register your input into the hook by invoking the "register" function */}
+          <label htmlFor="nama" className="label-input">
+            Nama Lengkap
+          </label>
+          <input
+            id="nama"
+            defaultValue=""
+            {...register("nama")}
+            className="input-text"
+          />
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+          <label htmlFor="ttl" className="label-input">
+            Tempat Tanggal Lahir
+          </label>
+          <input
+            type={"date"}
+            id="ttl"
+            defaultValue={today}
+            {...register("ttl", { required: true })}
+            className="input-text"
+          />
 
-      <input type="submit" />
-    </form>
+          {/* include validation with required or other standard HTML validation rules */}
+          <input {...register("exampleRequired", { required: true })} />
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
+
+          <input type="submit" className="btn" />
+        </div>
+      </form>
+    </>
   );
 };
 
